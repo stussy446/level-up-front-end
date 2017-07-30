2baaff7aa97fddbd65beb98eec8ff5fe352ea20f
Yun Jae Chang <yunjae0500@yahoo.com>
2017-07-30 14:21:52 -0700

    Set up routes for the sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :users
+  resources :sessions, only: [:new, :delete]
+
+
+
+  get '/login', to: 'sessions#new', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
+
 end
