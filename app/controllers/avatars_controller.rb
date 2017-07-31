8cd7bddd1eb0f988d2f22a862a09797e1f1335ec
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-07-30 23:58:24 -0700

    added avatars views and controllers

---
+++ app/controllers/avatars_controller.rb
@@ -0,0 +1,18 @@
+class AvatarsController < ApplicationController
+  include HTTParty
+  def index
+    response =
+    HTTParty.get("http://localhost:3000/api/avatars")
+    puts response
+    @names = response['avatars'].map do |n|
+      n
+    end
+  end
+
+  def show
+    response =
+    HTTParty.get("http://localhost:3000/api/users/1/avatars/1")
+    puts response
+    @avatar = response['avatar']
+  end
+end
