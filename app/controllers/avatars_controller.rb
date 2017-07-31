b9e4226a7e70dbd597d2443bd859aaf562b46a28
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-07-31 14:23:18 -0700

    changing routes

--- app/controllers/avatars_controller.rb
+++ app/controllers/avatars_controller.rb
@@ -3,7 +3,7 @@ class AvatarsController < ApplicationController
   include HTTParty
   def index
     response =
-    HTTParty.get("http://localhost:3001/api/avatars")
+    HTTParty.get("http://level-up-api.herokuapp.com/api/avatars")
     puts response
     @names = response['avatars'].map do |n|
       n
@@ -12,7 +12,7 @@ class AvatarsController < ApplicationController
 
   def show
     response =
-    HTTParty.get("http://localhost:3001/api/users/1/avatars/1")
+    HTTParty.get("http://level-up-api.herokuapp.com/api/users/1/avatars/1")
     puts response
     @avatar = response['avatar']
     @completed_achievements = response['completed_achievements']
