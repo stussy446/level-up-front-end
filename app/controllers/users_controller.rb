a072c08c239bcd630510040ce7ddcb9457b29a80
Steve Rector <steverector27@gmail.com>
2017-08-01 10:46:31 -0700

    user show route getting data correctly

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,10 @@ class UsersController < ApplicationController
   include UsersHelper
   def index
     @users = HTTParty.get("http://localhost:3001/api/users")
-    puts @users
   end
+  
   def show
+    @user = HTTParty.get("http://localhost:3001/api/users/#{params[:id]}")
+    p @user
   end
 end
