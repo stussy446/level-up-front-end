377fd47bf5190a12929c81f024d0d620671a3a6c
Steve Rector <steverector27@gmail.com>
2017-08-01 10:34:41 -0700

    getting users data from backend successfully

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   include UsersHelper
   def index
-    @result = current_user
+    @users = HTTParty.get("http://localhost:3001/api/users")
+    puts @users
   end
   def show
   end
