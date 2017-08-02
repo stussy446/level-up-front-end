3435dad74073a2c84fa50eb1fba48c18c4bee5c3
steve rector <steverector27@gmail.com>
2017-08-02 14:44:25 -0700

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -14,5 +14,10 @@ ActiveRecord::Schema.define(version: 20170730203835) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  
+  create_table "users", force: :cascade do |t|		
+     t.datetime "created_at", null: false		
+     t.datetime "updated_at", null: false		
+   end
 
 end
