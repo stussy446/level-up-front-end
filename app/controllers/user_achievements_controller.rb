c35a6d40e347103c55e7a4ebf66f01b34a16065b
Jarrade Pierce <slim7@Jarrades-MacBook-Air.local>
2017-08-01 21:16:33 -0700

    created add achievements button in achievments show

--- app/controllers/user_achievements_controller.rb
+++ app/controllers/user_achievements_controller.rb
@@ -6,7 +6,14 @@ class UserAchievementsController < ApplicationController
     @achievement_data = @response["achievement_data"]
   end
 
+  def create
+  #create_user_achievement = HTTParty.post("http://localhost:3001/api/users/1/user_achievements")
+  #redirect_to 
+  end
+
   def show
+    # @response = HTTParty.post("http://localhost:3001/api/users/1/user_achievements/#{params[:id]}")
+    # puts @response
   end
 
   def edit
